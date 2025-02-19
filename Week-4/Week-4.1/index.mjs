4b38c0a8356c322220d49f2b6d563f3fae7e4627
Gaurav Mehra <gmehra161@gmail.com>
2025-02-20 00:01:30 +0530

    sort files

--- Week-4/Week-4.1/index.mjs
+++
@@ -1,6 +0,0 @@
-import chalk from 'chalk'; //modern import syntax
-
-console.log(chalk.red("hello world"));
-console.log(chalk.blue('Hello, world!'));
-console.log(chalk.red.bold('This is an error message.'));
-console.log(chalk.green.underline('This is a success message.'));
